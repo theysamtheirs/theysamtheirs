15197137c469e4f41c6446639a8f22b2268b99b6
Sam Hankins <12632662+IAmSamHankins@users.noreply.github.com>
2022-09-07 23:46:10 -0400

    updates typography, container widths, and other assorted visual tweaks.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		fontFamily: {
+		},
+		extend: {
+			fontFamily: {
+				'brand': ['gastromond', 'sans-serif'],
+				'sans': ['articulat-cf', 'sans-serif'],
+			},
+		},
 	},
 	plugins: [],
 }
